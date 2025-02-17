82902e1899d12238c9198ab3a73492d967da9dd3
Finian Landes <finian.landes@bluewin.ch>
2025-02-17 18:05:01 +0100

    Added ResVAE

--- Pytorch/Train.py
+++ Pytorch/Train.py
@@ -13,13 +13,13 @@ from Utils import *
 from Conf import *
 
 batch_size: int = 2
-epochs: int = 200
+epochs: int = 10
 learning_rate: float = 1e-6
 lr_decay: int = 40
 lr_gamma: float = 0.1
 reprod_loss_weight: float = 20000
 logging_level: int = logging.INFO
-model_name: str = "audio_vae_v3_small"
+model_name: str = "audio_res_vae_v1_small"
 training_data_name: str = "training_v1"
 
 
@@ -32,10 +32,10 @@ logger.info(f"Data loaded with shape: {file.shape}")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model = VAE(in_channels=1, latent_dim=512, device=device,input_shape=[0,0, file.shape[-2], file.shape[-1]]).to(device)
+model = ResVAE(in_channels=1, latent_dim=512, device=device,activation=nn.GELU(),input_shape=[0,0, file.shape[-2], file.shape[-1]]).to(device)
 if os.path.exists(f"{MODEL_PATH}/{model_name}.pth"):
     model.load_state_dict(torch.load(f"{MODEL_PATH}/{model_name}.pth"))
-optimizer = optim.Adam(model.parameters(), lr=1e-5)
+optimizer = optim.AdamW(model.parameters(), lr=1e-5)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=lr_decay, gamma=lr_gamma)
 x = train_VAE(model, data_loader, optimizer, loss_VAE, epochs=epochs, device=device, reprod_loss_weight=reprod_loss_weight)
 scatter_plot(x)
